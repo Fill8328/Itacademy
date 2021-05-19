5362e7abd72192e8c109c230146140a288e6f839
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-19 20:42:00 +0300

    HomeWorke

---
+++ src/fill/jma/Main.java
@@ -0,0 +1,13 @@
+package fill.jma;
+
+import fill.jma.lesson1.OperatorDemo;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        OperatorDemo operatorDemo = new OperatorDemo();
+        operatorDemo.printSumm();
+    }
+}
