7bc97e6172c5c1c938fecc3ee8ccdd8fb73ce012
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-20 19:43:16 +0300

    Cercle Rectangle

--- src/fill/jma/lesson2/CircleRectangle.java
+++ src/fill/jma/lesson2/CircleRectangle.java
@@ -2,7 +2,9 @@ package fill.jma.lesson2;
 
 public class CircleRectangle {
     public void circlRectangle() {
-        double a = 9, b = 9, r = 5;
+        double b = 9;
+        double a = 9;
+        double r = 5;
         double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
         System.out.println("диагональ прямоугольника с=" + c);
         if (c > r) {
