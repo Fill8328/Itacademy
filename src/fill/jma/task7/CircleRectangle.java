6555a0c444169d2a121957a6c74f2fe716106a5a
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-21 09:52:41 +0300

    Circle Rectangle

--- src/fill/jma/task7/CircleRectangle.java
+++ src/fill/jma/task7/CircleRectangle.java
@@ -6,7 +6,7 @@ public class CircleRectangle {
         double a = 9;
         double r = 5;
         double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        System.out.println("диагональ прямоугольника с=" + c);
+        System.out.println("диагональ  прямоугольника с=" + c);
         if (c > r) {
             System.out.println("нужен больше круг");
         } else {
