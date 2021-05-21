826a66644c4f14a6205b1eec667dcf338dfed5db
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-21 09:54:49 +0300

    factorial

--- src/fill/jma/task12/Factorial.java
+++ src/fill/jma/task12/Factorial.java
@@ -1,12 +1,12 @@
 package fill.jma.task12;
 
 public class Factorial {
-    public void factorial(){
-    int sum = 1;
-    int i = 1;
-    for(; i <=10; i++){
-        sum = sum * i;
+    public void factorial() {
+        int sum = 1;
+        int i = 1;
+        for (; i <= 10; i++) {
+            sum = sum * i;
         }
         System.out.println(sum);
-}
+    }
 }
