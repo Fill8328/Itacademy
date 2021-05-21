31327cae6861e63084b76779f205149f0bf920b4
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-21 11:19:53 +0300

    hope seven number

---
+++ src/fill/jma/task16/HopeNum.java
@@ -0,0 +1,14 @@
+package fill.jma.task16;
+
+public class HopeNum {
+    public void hope() {
+        int Number = 0;
+        int range = 100;
+        for (int i = 1; i <= range; i++) {
+            Number++;
+            if (Number % 7 == 0) {
+                System.out.println("Hope " + Number);
+            }
+        }
+    }
+}
