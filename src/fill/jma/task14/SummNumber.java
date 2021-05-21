a60635af2785186b7bbe71cf36babce24c3efe50
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-21 10:47:38 +0300

    Sum Number

--- src/fill/jma/task14/SummNumber.java
+++ src/fill/jma/task14/SummNumber.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SummNumber {
-    public void summNumber() throws IOException {
+    public void sumNumber() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int Number = Integer.parseInt(String.valueOf(reader.readLine()));
-        int Summ = 0;
+        int Sum = 0;
         int num;
         int i = 0;
         do {
             num = Number % 10;
-            Summ = Summ + num;
+            Sum = Sum + num;
             Number = Number / 10;
             i++;
         }
         while (i < 50);
-        System.out.println(Summ);
+        System.out.println(Sum);
     }
 }
