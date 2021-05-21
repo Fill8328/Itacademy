c65c09da40a165d21729e712ba71a69980ff10e8
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-21 11:20:12 +0300

    it akademy

--- src/fill/jma/Main.java
+++ src/fill/jma/Main.java
@@ -1,10 +1,6 @@
 package fill.jma;
 
-import fill.jma.task12.Factorial;
-import fill.jma.task13.Factorialcyclewhile;
-import fill.jma.task14.SummNumber;
-import fill.jma.task6.OperatorDemo;
-import fill.jma.task7.CircleRectangle;
+import fill.jma.task16.HopeNum;
 
 import java.io.IOException;
 
@@ -18,8 +14,11 @@ public class Main {
         Factorial factorial = new Factorial();
         factorial.factorial();
         Factorialcyclewhile factorialcyclewhile = new Factorialcyclewhile();
-        factorialcyclewhile.CycleWhile();*/
+        factorialcyclewhile.CycleWhile();
         SummNumber sumNumber = new SummNumber();
-        sumNumber.sumNumber();
+        sumNumber.sumNumber();*/
+        HopeNum hopeNum = new HopeNum();
+        hopeNum.hope();
+
     }
 }
