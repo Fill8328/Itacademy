9cff3f25d9b2730fb47eba613e233d80e3baacf5
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-21 10:50:58 +0300

    it academy

--- src/fill/jma/Main.java
+++ src/fill/jma/Main.java
@@ -2,6 +2,7 @@ package fill.jma;
 
 import fill.jma.task12.Factorial;
 import fill.jma.task13.Factorialcyclewhile;
+import fill.jma.task14.SummNumber;
 import fill.jma.task6.OperatorDemo;
 import fill.jma.task7.CircleRectangle;
 
@@ -10,13 +11,15 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        OperatorDemo operatorDemo = new OperatorDemo();
+        /*OperatorDemo operatorDemo = new OperatorDemo();
         operatorDemo.printSumm();
         CircleRectangle circleRectangle = new CircleRectangle();
         circleRectangle.circlRectangle();
         Factorial factorial = new Factorial();
         factorial.factorial();
         Factorialcyclewhile factorialcyclewhile = new Factorialcyclewhile();
-        factorialcyclewhile.CycleWhile();
+        factorialcyclewhile.CycleWhile();*/
+        SummNumber sumNumber = new SummNumber();
+        sumNumber.sumNumber();
     }
 }
