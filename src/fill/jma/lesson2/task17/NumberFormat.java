23eae5d51972dd94ad37c3af6ad7339a09f527cf
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-23 16:47:50 +0300

    Number Format

--- src/fill/jma/lesson2/task17/NumberFormat.java
+++ src/fill/jma/lesson2/task17/NumberFormat.java
@@ -1,29 +1,36 @@
 package fill.jma.lesson2.task17;
 
+import java.io.BufferedReader;
+
 public class NumberFormat {
     public void Format() {
-        int number = 1111200133;
+        int number = 14342493;
         int rank = 0;
         int remnant = 0;
         int length = (int) (Math.log10(number) + 1);
-        //  System.out.println(length);
         remnant = (length % 3);
         rank = length - remnant;
         int counter = (rank / 3);
         int FerstRank = (int) (number / Math.pow(10, rank));
         if (FerstRank == 0) {
             System.out.print("");
-        }
-        else {
+        } else {
             System.out.print(FerstRank + " ");
         }
         int OtherRank = 0;
-        for (; counter > 0; ) {
+        while (counter > 0) {
             rank = rank - 3;
             OtherRank = (int) ((number / Math.pow(10, rank)) % 1000);
-            System.out.print(OtherRank + " ");
+            if (OtherRank == 0)
+                System.out.print("000 ");
+            else if (OtherRank < 10) {
+                System.out.print("00" + OtherRank + " ");
+            } else if (OtherRank <= 99) {
+                System.out.print("0" + OtherRank + " ");
+            } else {
+                System.out.print(OtherRank + " ");
+            }
             counter--;
-            //  System.out.println(counter + " counter");
         }
     }
 }
