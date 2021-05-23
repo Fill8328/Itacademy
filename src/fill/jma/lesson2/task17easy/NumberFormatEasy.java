57a28c5665a279203db67b8c8a375e7604392ae3
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-23 20:25:05 +0300

    Number Format

---
+++ src/fill/jma/lesson2/task17easy/NumberFormatEasy.java
@@ -0,0 +1,13 @@
+package fill.jma.lesson2.task17easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberFormatEasy {
+    public void FormatEasy() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long number = Integer.parseInt(String.valueOf(reader.readLine()));
+        System.out.printf("%,d", number);
+    }
+}
