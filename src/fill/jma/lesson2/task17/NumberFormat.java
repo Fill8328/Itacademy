315df59aa26988fbad8930f76e5ab98f175e609a
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-23 16:21:25 +0300

    Number Format

--- src/fill/jma/lesson2/task17/NumberFormat.java
+++ src/fill/jma/lesson2/task17/NumberFormat.java
@@ -1,17 +1,31 @@
 package fill.jma.lesson2.task17;
 
 public class NumberFormat {
-    public void Format(){
-        int number = 20023143;
-        int thousand = 1000;
-        int tem;
-        for (int i = 1;i <=4;i++){
-            tem = number%thousand;
-            number=number/thousand;
-            String s=String.valueOf(tem);
-            System.out.println(s);
+    public void Format() {
+        int number = 1011200133;
+        int rank = 0;
+        int remnant = 0;
+        int length = (int) (Math.log10(number) + 1);
+        //  System.out.println(length);
+        remnant = (length % 3);
+        rank = length - remnant;
+        int counter = (rank / 3);
+        int FerstRank = (int) (number / Math.pow(10, rank));
+        if (FerstRank == 0) {
+            System.out.print("");
         }
-
-
+        else {
+            System.out.print(FerstRank + " ");
+        }
+        int OtherRank = 0;
+        for (; counter > 0; ) {
+            rank = rank - 3;
+            OtherRank = (int) ((number / Math.pow(10, rank)) % 1000);
+            System.out.print(OtherRank + " ");
+            counter--;
+            //  System.out.println(counter + " counter");
+        }
     }
 }
+
+
