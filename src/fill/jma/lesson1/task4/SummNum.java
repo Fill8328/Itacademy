f865f60bfbea3ef817ee496106d84c9dda84d186
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-24 08:21:54 +0300

    Sum Number easy

---
+++ src/fill/jma/lesson1/task4/SummNum.java
@@ -0,0 +1,18 @@
+package fill.jma.lesson1.task4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SummNum {
+    public void summNum() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Write you number ");
+        System.out.print("a=");
+        int a = Integer.parseInt(reader.readLine());
+        System.out.print("b=");
+        int b = Integer.parseInt(reader.readLine());
+        int c = (a + b) + (a * b);
+        System.out.println("(a+b)+(a*b) = " + c);
+    }
+}
