b213ef1e401d1041e0faa1b12dbaa28f031cefd0
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-05-26 18:43:48 +0300

    Prime Number

---
+++ src/fill/jma/lesson2/task15/PrimeNumber.java
@@ -0,0 +1,21 @@
+package fill.jma.lesson2.task15;
+
+public class PrimeNumber {
+    public void primenumber() {
+        boolean flag =false;
+        int count = 0;
+        for (int i = 50; i <= 70; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i%j==0){
+
+                }
+
+
+            }
+        }
+    }
+}
+
+
+
+
