2aad1ffb418a2ae8388104989e0778391d843c78
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-06-29 10:01:21 +0300

    it academy

--- src/fill/jma/Lesson7/ReversNumbersText.java
+++ src/fill/jma/Lesson7/ReversNumbersText.java
@@ -1,14 +1,17 @@
 package fill.jma.Lesson7;
 
-import java.io.*;
+import java.beans.Customizer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReversNumbersText {
 
-    public static List<Integer> readIntegersFromFile(String path){
+    public static List<Integer> readIntegersFromFile(String path) {
 
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             ArrayList<Integer> collection = new ArrayList<>();
 
             String result = bufferedReader.readLine();
@@ -17,21 +20,18 @@ public class ReversNumbersText {
                 result = bufferedReader.readLine();
             }
             return collection;
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
-        catch (IOException e){
-                throw new RuntimeException();
+        catch (NumberFormatException formatException){
+            throw new RuntimeException("Uncorrected format data", formatException);
         }
     }
 
-//    public static List<Integer> reversListElement (ArrayList<Integer> reversNum){
-//        return null;
-//    }
-
-
-    public static List<Integer> reversList (List<Integer> list){
+    public static List<Integer> reversList(List<Integer> list) {
         ArrayList<Integer> collectionTwo = new ArrayList<>();
         Integer summ = 0;
-        while (summ<list.size()){
+        while (summ < list.size()) {
             ++summ;
             collectionTwo.add(list.get(list.size() - summ));
         }
